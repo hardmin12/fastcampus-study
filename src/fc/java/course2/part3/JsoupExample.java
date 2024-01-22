1983736551b8de4c400caa8a0b14d220df8df4d8
hardmin <hardmin1222@gmail.com>
2024-01-22 16:02:58 +0900

    Jsoup API 활용하기

---
+++ src/fc/java/course2/part3/JsoupExample.java
@@ -0,0 +1,33 @@
+package fc.java.course2.part3;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class JsoupExample {
+    public static void main(String[] args) {
+        String url = "https://sum.su.or.kr:8888/bible/today?base_de=2023-03-20";
+
+        try {
+            Document document = Jsoup.connect(url).get(); //url이 잘못된 url 일 수 있기 떄문에 예외 처리 필요
+
+            Element bibleText = document.getElementById("bible_text");
+            Element bibleinfoBox = document.getElementById("bibleinfo_box");
+
+            System.out.println("bibleText = " + bibleText.text());
+            System.out.println("bibleinfoBox = " + bibleinfoBox.text());
+
+            Elements num = document.select(".num");
+            Elements info = document.select(".info");
+
+            for(int i = 0; i<num.size(); i++) {
+                System.out.println(num.get(i).text()+":"+info.get(i).text());
+            }
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
